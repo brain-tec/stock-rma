ca9c54f0ea2c9eef5f478670398ec85f03293cc6
AaronHForgeFlow <ahenriquez@eficent.com>
2022-03-04 10:56:07 +0100

    [FIX] rma: rma_custmer_user has no write permissions in partner, so compute method fails. [IMP] rma: use rma user in tests [FIX] rma_account: move_line_id field string [IMP] rma, rma_account, rma_sale, rma_purchase: tests for stock valuation [FIX] account_move_line_rma_order_line: minor lint, make auto-install

--- rma/models/res_partner.py
+++ rma/models/res_partner.py
@@ -1,4 +1,4 @@
-# Copyright 2017 ForgeFlow
+# Copyright 2017-22 ForgeFlow
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
@@ -9,12 +9,14 @@ class ResPartner(models.Model):
 
     def _compute_rma_line_count(self):
         for rec in self:
-            rec.rma_line_count = len(rec.rma_line_ids)
+            rec.sudo().rma_line_count = len(rec.rma_line_ids)
 
     rma_line_ids = fields.One2many(
         comodel_name="rma.order.line", string="RMAs", inverse_name="partner_id"
     )
-    rma_line_count = fields.Integer(compute="_compute_rma_line_count")
+    rma_line_count = fields.Integer(
+        compute="_compute_rma_line_count", compute_sudo=True
+    )
 
     def action_open_partner_rma(self):
         action = self.env.ref("rma.action_rma_customer_lines")
