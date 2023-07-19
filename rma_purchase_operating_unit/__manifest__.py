b103a0f26d05eacfc30ad7f685ec9ef9fd5c891d
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-07-19 17:34:20 +0530

    [MIG] rma_purchase_operating_unit: Migration to 15.0

--- rma_purchase_operating_unit/__manifest__.py
+++ rma_purchase_operating_unit/__manifest__.py
@@ -1,14 +1,14 @@
-# © 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2023 ForgeFlow S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "RMA Purchase with Operating Units",
-    "version": "12.0.1.0.0",
-    "author": "Eficent," "Odoo Community Association (OCA)",
-    "license": "LGPL-3",
+    "version": "15.0.1.0.0",
+    "author": "ForgeFlow," "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
     "website": "https://github.com/ForgeFlow/stock-rma",
     "category": "RMA",
-    "depends": ["rma_operating_unit", "rma_purchase"],
+    "depends": ["rma_operating_unit", "rma_purchase", "purchase_operating_unit"],
     "data": [],
     "installable": True,
 }
