c5e361977b2062505239244aee0415af17a2364e
AaronHForgeFlow <aaron.henriquez@forgeflow.com>
2024-08-01 17:55:17 +0200

    [ADD] rma_repair_put_away: glue module between rma_repair & rma_put_away
    When not using rma_put_away you can define push rules anyway to move the products
    to the repair location, and those transfers count as rma repair transfers
    
    if you install and use rma_put_away then the rma repair transfers are the ones
    from the putaway moves
    
    Before this commit there is a dependency issue because the rma_repair code
    is using a field declared in the rma_put_away, this commit fixes that issue

---
+++ rma_repair_put_away/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "RMA Repair Put Away",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "RMA",
+    "summary": "RMA repairs with Put away",
+    "author": "ForgeFlow",
+    "website": "https://github.com/ForgeFlow/stock-rma",
+    "depends": ["rma_repair", "rma_put_away"],
+    "installable": True,
+    "auto_install": True,
+}
